0195de6533a0bc5d364a85a48204a170edf0e681
Kendra Fitzgerald <kendrajfitzgerald@gmail.com>
2023-04-02 17:55:11 -0400

    built fetch requestwith API key. had to not use previous variables because it did not work for some reason, put API directly into fetch request

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,25 @@ $( function() {
   } );
 
   var APIKey= "0ca98b6a40434df572129e17e48d6ead";
-  var cityName;
   var date;
   var temp;
   var humidity;
   var windSpeed;
   var icon;
 
-  var currentWeatherUrl= "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+  var searchButton = document.querySelector(".btn");
+
+  var searchName= document.querySelector("#inputCityName2");
+
+
+  function getWeather (city) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=0ca98b6a40434df572129e17e48d6ead&units=imperial").then(function(response){
+      return response.json()
+    }).then(function(data){
+      console.log(data)
+    })
+  }
+
+  searchButton.addEventListener("click", function(event){
+    getWeather(searchName.value);
+  })
