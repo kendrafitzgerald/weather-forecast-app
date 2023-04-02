d3b45ce6366c1ac07a0804c6ab24ebd819c0539e
Kendra Fitzgerald <kendrajfitzgerald@gmail.com>
2023-04-01 20:12:16 -0400

    added prelim variables and API link

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
 $( function() {
     $( "#selectable" ).selectable();
   } );
+
+  var APIKey= "0ca98b6a40434df572129e17e48d6ead";
+  var cityName;
+  var date;
+  var temp;
+  var humidity;
+  var windSpeed;
+  var icon;
+
+  var queryUrl= "var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
