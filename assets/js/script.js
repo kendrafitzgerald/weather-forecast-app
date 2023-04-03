4bd63370fde1e8c5e298a13ab8db382a610d3714
Kendra Fitzgerald <kendrajfitzgerald@gmail.com>
2023-04-03 16:17:26 -0400

    added icons to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,30 +47,54 @@
    wind.textContent= "Wind: " + data.list[0].wind.speed + " MPH";
    humidity.textContent="Humidity: " + data.list[0].main.humidity + "%";
 
+    var iconOne = document.createElement("img");
+    iconOne.setAttribute("src", "https://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + "@2x.png");
+    cityName.appendChild(iconOne);
+
    dayOneDate.textContent= data.list[3].dt_txt.slice(0, 11);
    dayOneTemp.textContent= "Temp: " + data.list[3].main.temp+ " °F";
    dayOneWind.textContent= "Wind: " + data.list[3].wind.speed + " MPH";
    dayOneHumidity.textContent= "Humidity: " + data.list[3].main.humidity + "%";
 
+   var iconTwo= document.createElement("img");
+   iconTwo.setAttribute("src", "https://openweathermap.org/img/wn/" + data.list[3].weather[0].icon + "@2x.png");
+   dayOneDate.appendChild(iconTwo);
+
    dayTwoDate.textContent= data.list[11].dt_txt.slice(0, 11);
    dayTwoTemp.textContent= "Temp: " + data.list[11].main.temp+ " °F";
    dayTwoWind.textContent= "Wind: " + data.list[11].wind.speed + " MPH";
-   dayTwoHumidity.textContent= "Humidity: " + data.list[19].main.humidity + "%";
+   dayTwoHumidity.textContent= "Humidity: " + data.list[11].main.humidity + "%";
+
+   var iconThree= document.createElement("img");
+   iconThree.setAttribute("src", "https://openweathermap.org/img/wn/" + data.list[11].weather[0].icon + "@2x.png");
+   dayTwoDate.appendChild(iconThree);
 
    dayThreeDate.textContent= data.list[19].dt_txt.slice(0, 11);
    dayThreeTemp.textContent= "Temp: " + data.list[19].main.temp+ " °F";
    dayThreeWind.textContent= "Wind: " + data.list[19].wind.speed + " MPH";
    dayThreeHumidity.textContent= "Humidity: " + data.list[19].main.humidity + "%";
 
+   var iconFour= document.createElement("img");
+   iconFour.setAttribute("src", "https://openweathermap.org/img/wn/" + data.list[19].weather[0].icon + "@2x.png");
+   dayThreeDate.appendChild(iconFour);
+
    dayFourDate.textContent= data.list[27].dt_txt.slice(0, 11);
    dayFourTemp.textContent= "Temp: " + data.list[27].main.temp+ " °F";
    dayFourWind.textContent= "Wind: " + data.list[27].wind.speed + " MPH";
    dayFourHumidity.textContent= "Humidity: " + data.list[27].main.humidity + "%";
 
+   var iconFive= document.createElement("img");
+   iconFive.setAttribute("src", "https://openweathermap.org/img/wn/" + data.list[27].weather[0].icon + "@2x.png");
+   dayFourDate.appendChild(iconFive);
+
    dayFiveDate.textContent= data.list[35].dt_txt.slice(0, 11);
    dayFiveTemp.textContent= "Temp: " + data.list[35].main.temp+ " °F";
    dayFiveWind.textContent= "Wind: " + data.list[35].wind.speed + " MPH";
    dayFiveHumidity.textContent= "Humidity: " + data.list[35].main.humidity + "%";
+
+   var iconSix= document.createElement("img");
+   iconSix.setAttribute("src", "https://openweathermap.org/img/wn/" + data.list[35].weather[0].icon + "@2x.png");
+   dayFiveDate.appendChild(iconSix);
 
   console.log(data)
     
